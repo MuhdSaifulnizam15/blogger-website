7e52c40811e069b2b3423e86aa625923a707c3dd
Muhd Saifulnizam <muhdsaifulnizam1998@gmail.com>
2020-03-22 00:57:09 +0800

    Added Site Content, Blog Carousel, Sidebar and Post Content

--- js/main.js
+++ js/main.js
@@ -9,4 +9,13 @@ $(document).ready(function(){
         $nav.toggleClass('collapse');
     })
 
+    // Owl Carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    });
 });
