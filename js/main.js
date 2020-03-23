05e565d28787ae0c84c09296e2e6313006e046c2
Muhd Saifulnizam <muhdsaifulnizam1998@gmail.com>
2020-03-24 00:56:42 +0800

    Full Responsive Website

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
 
 $(document).ready(function(){
 
@@ -16,6 +30,15 @@ $(document).ready(function(){
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
+
+    // Click to scroll to top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
 });
